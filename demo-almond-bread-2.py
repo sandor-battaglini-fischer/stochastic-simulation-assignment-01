9bc8a5a5564e692a81e74beffcd33b761d3dd9d1
Sándor Battaglini-Fischer <77168983+sandor-battaglini-fischer@users.noreply.github.com>
2023-11-17 18:03:03 +0100

    Delete demo-almond-bread-2.py

--- demo-almond-bread-2.py
+++
@@ -1,8 +0,0 @@
-def mandelbrot_set(xmin, xmax, ymin, ymax, width, height, max_iter):
-    r1 = np.linspace(xmin, xmax, width)
-    r2 = np.linspace(ymin, ymax, height)
-    n3 = np.empty((width, height))
-    for i in range(width):
-        for j in range(height):
-            n3[i, j] = mandelbrot(r1[i] + 1j*r2[j],max_iter)
-    return (r1, r2, n3)
